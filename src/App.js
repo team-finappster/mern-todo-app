f1ebc108a22df159aa6d7649ff9325714b63bf3d
mikegjeff <mikegjeffcoat@gmail.com>
2019-08-21 14:02:08 +1200

    creat todo improved

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
             <div className="collpase navbar-collapse">
